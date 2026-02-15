cf01b3244211e516bbb1fc96a26c1f1c59c072f7
AAdelMM <ahmed.adel426@gmail.com>
2026-02-15 16:48:54 +0200

    add videos section4

--- resources/views/sections/video.blade.php
+++ resources/views/sections/video.blade.php
@@ -1,4 +1,4 @@
-<section id="videos_lib" class="py-16 bg-gray-400">
+<section id="videos_lib" class="py-16 bg-black text-white">
 
     <div class="section-header">
         <h2>مركز المعرفة المرئية</h2>
