95162a596020850100c8536451b4a2ab7d2c431c
AAdelMM <ahmed.adel426@gmail.com>
2026-02-15 16:11:42 +0200

    edit footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="bg-gray-900 py-12 px-6">
-  <div class="container mx-auto max-w-6xl">
-    <div class="grid md:grid-cols-4 gap-8">
-      <div>
+  <div class="container mx-auto ">
+    <div class="footer-content flex flex-col md:flex-row justify-between items-start gap-8">
+      <div class="footer_description">
         <img src="{{asset('images/light_logo.png')}}" alt="Logo" class="h-10 mb-4">
         <p class="text-gray-300">{{ __('messages.footer_description') }}</p>
       </div>
       
-      <div>
+      <div class="footer_quick_links">
         <h4 class="text-lg font-bold mb-4">{{ __('messages.quick_links') }}</h4>
         <ul class="space-y-2">
           <li><a href="#home" class="text-gray-300 hover:text-white transition duration-300">{{ __('messages.home') }}</a></li>
@@ -16,7 +16,7 @@
         </ul>
       </div>
       
-      <div>
+      <div class="footer_contact">
         <h4 class="text-lg font-bold mb-4">{{ __('messages.contact_us') }}</h4>
         <ul class="space-y-2 text-gray-300">
           <li>{{ __('messages.email') }}: info@najabh.com</li>
@@ -25,9 +25,9 @@
         </ul>
       </div>
       
-      <div>
+      <div class="footer_social">
         <h4 class="text-lg font-bold mb-4">{{ __('messages.follow_us') }}</h4>
-        <div class="flex">
+        <div class="icons">
          
           <a href="#" class="text-gray-300 hover:scale-110 transition duration-300 mx-2 bg-amber-50 border rounded-[5px] h-[50px] w-[50px] flex items-center justify-center"><img src="{{asset('images/twitter.png')}}" alt="Twitter"></a>
           <a href="#" class="text-gray-300 hover:scale-110 transition duration-300 mx-2  h-[50px] w-[50px] flex items-center justify-center"><img src="{{asset('images/insta.png')}}" alt="Instagram"></a>
