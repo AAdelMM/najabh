27cd235add14bd7c340f2c2e17a459f5a6f345ef
AAdelMM <ahmed.adel426@gmail.com>
2026-02-22 12:36:59 +0200

    add favico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', __('messages.hero_title'))</title>
+  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   
