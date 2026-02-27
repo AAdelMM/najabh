6a0d605369785fe796e452e3d76f50aeecfabf49
AAdelMM <ahmed.adel426@gmail.com>
2026-02-27 13:19:59 +0200

    update userfile

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 //use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Panel;
-
-class User extends Authenticatable
+use Filament\Panel; 
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
